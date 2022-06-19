44bc74898a9b2039e45bfc0f89eb3de0ef4e645e
Alper Çıtak <citak.alper@gmail.com>
2022-06-19 19:10:52 +0300

    added users route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  async getUsers(): Promise<User[]> {
+    return await this.prismaService.user.findMany();
   }
 }
